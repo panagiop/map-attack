49583567fc9d47a6c30806920a599a5bd678bed4
panagiop <panagiotis8panagiotopoulos@gmail.com>
2015-10-24 18:37:34 +0300

    change generalconfig process.env.MONGO_URL

--- server/generalconfig.js
+++ server/generalconfig.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = {
-	db: process.env.db || 'localhost/mapattack',
+	db: process.env.MONGO_URL || 'localhost/mapattack',
 	secretJWTToken: process.env.secretJWTToken || 'Basd!6$@@d01S*'
 }
