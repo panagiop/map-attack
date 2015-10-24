e4c454e2ce2f5023e7d297b850ac2a1c19ff878d
panagiop <panagiotis8panagiotopoulos@gmail.com>
2015-10-24 19:08:22 +0300

    remove app.listen() default from app.js

--- server/app.js
+++ server/app.js
@@ -61,8 +61,5 @@ app.use(passport.initialize());
 if ('development' == app.get('env')) {
     app.use(errorHandler());
 } 
-// app.listen(app.get('port'), function() {
-//     console.log('Express server listening on port ' + app.get('port'));
-// });
- 
+
 exports = module.exports = app;
