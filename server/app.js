7bd2d050d0ee6c2fc422c9755aa0fd7f495cc987
panagiop <panagiotis8panagiotopoulos@gmail.com>
2015-10-24 19:03:37 +0300

    remove favicon from app.js

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var io = require('socket.io').listen(server);
 
 var path = require('path'); 
 
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var methodOverride = require('method-override');
 var cookieParser = require('cookie-parser');
