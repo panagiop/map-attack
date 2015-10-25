4d1272a66a1a2ea99f031b7421125a0736a2c79a
panagiop <panagiotis8panagiotopoulos@gmail.com>
2015-10-25 11:20:34 +0200

    added a map in view-only page + changes in map style

--- client/public/js/posts/controllers/view.js
+++ client/public/js/posts/controllers/view.js
@@ -1,8 +1,14 @@
 (function() {
 	'use strict';
 
-	function ViewPostController($stateParams, ApiResource, typeOfAttackValues) {
+	function ViewPostController($scope, $stateParams, ApiResource, typeOfAttackValues) {
         var self = this; 
+        var infowindow, center;
+
+        $scope.$on('mapInitialized', function (event, map) {
+            $scope.objMap = map;
+        });
+
         self.typeOfAttackValues = typeOfAttackValues;
         self.typeOfAttackValues =  _.pluck(self.typeOfAttackValues, 'name').splice(0, 4);
 
@@ -15,9 +21,20 @@
                 console.log("something went wrong fetching this post");
             }
         });
+
+        self.showInfoWindowOnMouseClick = function (event, p) {
+            infowindow = new google.maps.InfoWindow();
+            center = new google.maps.LatLng(p.loc[0],p.loc[1]);
+
+            infowindow.setContent(
+                '<p>' + p.title + '<br>' + p.text + '</p>');
+
+            infowindow.setPosition(center); 
+            infowindow.open($scope.objMap);
+        };  
     }
 
-    ViewPostController.$inject = ['$stateParams', 'ApiResource', 'typeOfAttackValues'];
+    ViewPostController.$inject = ['$scope', '$stateParams', 'ApiResource', 'typeOfAttackValues'];
 
     angular.module('postApp.controllers.view', [])
         .controller('ViewPostController', ViewPostController)
