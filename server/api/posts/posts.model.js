0b007a63376868680950bb1eee7803b77a7e3a2f
panagiop <panagiotis8panagiotopoulos@gmail.com>
2015-12-01 20:27:14 +0200

    beautify js files (server) part1

--- server/api/posts/posts.model.js
+++ server/api/posts/posts.model.js
@@ -1,19 +1,28 @@
 'use strict';
 
 var mongoose = require('mongoose');
-var	Schema = mongoose.Schema;
+var Schema = mongoose.Schema;
 
 var PostsSchema = new Schema({
-	title: { type: String, unique: true },
-	text: String,
-	type_of_attack: String,
-	loc: {
-    	type: [],
-    	index: '2d'
-	},
-	date: {type: 'Date', default: Date.now},
-	isPublished: Boolean,
-	__user : { type: Schema.Types.ObjectId, ref: 'User' }
+    title: {
+        type: String,
+        unique: true
+    },
+    text: String,
+    type_of_attack: String,
+    loc: {
+        type: [],
+        index: '2d'
+    },
+    date: {
+        type: 'Date',
+        default: Date.now
+    },
+    isPublished: Boolean,
+    __user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
 });
 
 module.exports = mongoose.model('Posts', PostsSchema);
