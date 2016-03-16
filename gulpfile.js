18ec2b6566235a275144f9b1e87d806a467f95cd
panagiop <panagiotis8panagiotopoulos@gmail.com>
2016-03-16 21:16:36 +0200

    bower was added replacing previous libraries

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-var gulp = require('gulp'); 
+var gulp = require('gulp');
 var minifyHTML = require('gulp-minify-html');
 var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');    
+var concat = require('gulp-concat');
 var ngAnnotate = require('gulp-ng-annotate');
 var minifyCSS = require('gulp-minify-css');
 
@@ -14,56 +14,59 @@ var banner = ['/**',
     ''].join('\n');
 
 gulp.task('minifyjs', function() {
-  gulp.src([ 
-      'client/public/js/lib/angular.min.js',
-      'client/public/js/lib/angular-ui-router.min.js',
-      'client/public/js/lib/angular-resource.min.js',
-      'client/public/js/lib/*.js',
+  gulp.src([
+      'client/bower_components/angular/angular.js',
+      'client/bower_components/angular-ui-router/release/angular-ui-router.js',
+      'client/bower_components/angular-resource/angular-resource.js',
+    //   'client/public/js/lib/*.js',
       'client/public/js/app.js',
       'client/public/js/**/*.js',
-      'client/public/theme/assets/js/jquery.min.js',
-      'client/public/theme/assets/js/bootstrap.min.js',
-      'client/public/theme/assets/js/*.js'
+      'client/bower_components/jquery/dist/jquery.js',
+      'client/bower_components/bootstrap/dist/js/bootstrap.js',
+      'client/bower_components/socket.io-client/socket.io.js',
+      'client/bower_components/lodash/lodash.js',
+      'client/bower_components/ngmap/build/scripts/ng-map.min.js'
+    //   'client/public/theme/assets/js/*.js'
     ])
-    .pipe(concat('app.min.js')) 
+    .pipe(concat('app.min.js'))
     .pipe(ngAnnotate())
     .pipe(uglify())
     .pipe(gulp.dest('client/public/js'));
-}); 
+});
 
 // minify new or changed HTML pages
-gulp.task('minifyhtml', function() { 
+gulp.task('minifyhtml', function() {
   	var htmlSrc = [
       'client/views/partials/**/*.html',
       'client/public/js/common/directives/**/*.html'
     ],
       htmlDst = 'client/public/templates';
- 
-	gulp.src(htmlSrc) 
+
+	gulp.src(htmlSrc)
 		.pipe(minifyHTML())
 		.pipe(gulp.dest(htmlDst));
-}); 
+});
 
 gulp.task('minifycss', function() {
-  gulp.src([ 
+  gulp.src([
     'client/public/theme/assets/css/*.css',
-    'client/public/theme/assets/css/custom-style.css'
+    'client/public/theme/assets/css/custom-style.css',
+    'client/bower_components/bootstrap/dist/css/bootstrap.css'
   ])
     .pipe(concat('styles.min.css'))
     .pipe(minifyCSS({processImport: false}))
     .pipe(gulp.dest('client/public/css'));
-}); 
+});
 
 gulp.task('watch', function() {
   gulp.watch([
-    'client/public/css/cuctom-style.css', 
     'client/public/theme/assets/css/*.css',
     '!client/css/styles.min.css'
-    ], ['minifycss']); 
+    ], ['minifycss']);
 
   gulp.watch([
     'client/public/js/**/*.js',
-      'client/public/js/app.js'
+    'client/public/js/app.js'
   ], ['minifyjs']);
 
   gulp.watch([
--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var app = express();
 var server = app.listen(process.env.PORT || 3000);
 var io = require('socket.io').listen(server);
 
-var path = require('path'); 
+var path = require('path');
 
 // var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -17,9 +17,9 @@ var session = require('express-session');
 var mongoStore = require('connect-mongo')(session);
 var mongoose = require('mongoose');
 var passport = require('passport');
-var generalconfig = require('./generalconfig'); 
+var generalconfig = require('./generalconfig');
 
-// mongoose.connect('mongodb://localhost/mapattack'); 
+// mongoose.connect('mongodb://localhost/mapattack');
 mongoose.connect(generalconfig.db);
 
 // populate db with sample data
@@ -38,8 +38,8 @@ app.set('view engine', 'html');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(compress());
 app.use(logger('dev'));
-app.use(methodOverride()); 
-app.use(cookieParser()); 
+app.use(methodOverride());
+app.use(cookieParser());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -48,11 +48,12 @@ app.use(bodyParser.urlencoded({
 app.use(multer());
 
 app.use(express.static(path.join(clientpath, 'public'), { maxAge: 2628000000 }));
+app.use('/bower_components',  express.static(clientpath + '/bower_components', { maxAge: 2628000000 } ));
 //app.use(express.static(path.join(__dirname, 'public')));
 
 // requirements (routes and other stuff)
 require('./socketconfig')(io);
-require('./routes')(app);  
+require('./routes')(app);
 require('./auth/local/config/passport');
 
 app.use(passport.initialize());
@@ -60,6 +61,6 @@ app.use(passport.initialize());
 // error handling middleware should be loaded after the loading the routes
 if ('development' == app.get('env')) {
     app.use(errorHandler());
-} 
+}
 
 exports = module.exports = app;
